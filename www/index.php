a41aa61cb50ff5128b8c200766be863d5e780813
UfimtsevSS <ufimets48@mail.ru>
2015-02-26 18:08:14 +0300

    Создали function.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,29 @@
 <?php
 
-phpinfo(); echo 'Hello, world!';
+class SimpleClass
+{  // property declaration
+    public $var = 'A default value';
+   // method declaration
+    public function displayVar() {
+        echo $this->var;
+    }
+}
+
+$simpl = new SimpleClass;
+// $simpl->var = 'A default value';
+$simpl->displayVar();
+
+class Article
+{
+    public $title;
+    public $text;
+    public function view() {
+        echo '<h2>' . $this->title . '</h2>';
+        echo '<h3>' . $this->text . '</h3>';
+	}
+}
+
+$art = new Article;
+$art->title = 'The best news!';
+$art->text = 'The bad news?'; 
+$art->view(); 
