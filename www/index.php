964c786c6f8b07dd1556efc598f6b48b6fc9c2eb
UfimtsevSS <ufimets48@mail.ru>
2015-02-26 23:32:56 +0300

    Создали function.php

--- www/index.php
+++ www/index.php
@@ -13,7 +13,7 @@ $simpl = new SimpleClass;
 // $simpl->var = 'A default value';
 $simpl->displayVar();
 
-class Article
+class Article1
 {
     public $title;
     public $text;
@@ -23,7 +23,23 @@ class Article
 	}
 }
 
-$art = new Article;
+$art = new Article1;
 $art->title = 'The best news!';
 $art->text = 'The bad news?'; 
-$art->view(); 
+$art->view();
+echo'<br>';
+
+class Article
+{
+    public $title;
+    public $text;
+    public function __construct($title, $text) {
+        $this->title = $title;
+        $this->text = $text;
+    }
+}
+
+$article = new Article('Заголовок', 'Текст');
+var_dump($article);
+
+// $article->view();
