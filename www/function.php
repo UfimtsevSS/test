f6d44f097ba419d203eee88d2f6679e658323939
UfimtsevSS <ufimets48@mail.ru>
2015-02-26 23:33:14 +0300

    Создали function.php

--- www/function.php
+++ www/function.php
@@ -1,5 +1,6 @@
 <?php
-function()
-{
-
-}
+function myPr($x, $n, $m) {
+	if (0 == $n) {return $x;}
+    else {return myPr($x, $n - 1, $m) * (1 + ($m / 100) / 12);}
+	}
+echo '12000 руб на 12 месяцев 10% ==> ' . myPr(12000, 12, 10);
